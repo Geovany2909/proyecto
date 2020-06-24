891beed87442f371c962693e8aca1b409a022cfe
Geovany2909 <geovanimarroquin114@gmail.com>
2020-06-23 22:23:58 -0600

    Card User

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,5 +1,5 @@
 @auth
-@extends('admin.products.layouts.pantilla')
+@extends('admin.layouts.pantilla')
     @section('title')
     Inicio
     @endsection
@@ -7,7 +7,6 @@
 @section('content')
     <div id="wrapper">
         <div id="page-wrapper">
-
             <div class="row">
                 <div class="col-lg-12">
                     <h1>Dashboard</h1>
@@ -17,17 +16,28 @@
                     </ol>
                     <div class="alert alert-success alert-dismissable">
                         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                        Welcome to SB Admin by <a class="alert-link" href="http://startbootstrap.com">Start
-                            Bootstrap</a>! Feel free to use this template for your admin needs! We are using a few
-                        different plugins to handle the dynamic tables and charts, so make sure you check out the
-                        necessary documentation links provided.
+                        {{ auth()->user()->name }}
                     </div>
                 </div>
-            </div><!-- /.row -->
+            </div>
 
-        </div><!-- /#page-wrapper -->
 
-    </div><!-- /#wrapper -->
+    		<div class="col-md-4 text-center" style="margin: 0 auto; float: none; margin-bottom: 10%">
+    		    <div class="card profile-card-1">
+    		        <img src="https://images.pexels.com/photos/946351/pexels-photo-946351.jpeg?w=500&h=650&auto=compress&cs=tinysrgb" alt="profile-sample1" class="background"/>
+    		        <img src="/images/{{ auth()->user()->photo ? auth()->user()->photo : 'user-photo-default.png' }}" alt="profile-image" class="profile"/>
+                    <div class="card-content">
+                    <h2>{{ auth()->user()->name }}<small>Admin</small></h2>
+                    <h5><small>{{ auth()->user()->email }}</small></h5>
+                    <div class="icon-block"><a href="#"><i class="fa fa-facebook"></i></a><a href="#"> <i class="fa fa-twitter"></i></a><a href="#"> <i class="fa fa-google-plus"></i></a></div>
+                    </div>
+                </div>
+            </div>
+
+
+ </div>
+
+</div>
 @endsection
 
 @endauth
---
+++ resources/views/admin/layouts/pantilla.blade.php
@@ -0,0 +1,82 @@
+@auth
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard - @yield('title')</title>
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/sb-admin.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/table.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/alertify.min.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('css/themes/default.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/user.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('font-awesome/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <style>
+        .delete{
+            display: none;
+        }
+    </style>
+</head>
+<body>
+   <!-- Sidebar -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="{{ route('home') }}">Ortopedia Velasquez</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav side-nav">
+                    <li><a href="{{ route('home') }}"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+                    <li><a href="{{ route('products.index') }}"><i class="fa fa-table"></i> Products</a></li>
+                    <li><a href="{{ route('products.create') }}"><i class="fa fa-edit"></i> Create Products</a></li>
+                </ul>
+
+                <ul class="nav navbar-nav navbar-right navbar-user">
+
+                    <li class="dropdown user-dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i>
+                            {{ Auth::user()->name }} <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#"><i class="fa fa-user"></i> Profile</a></li>
+                            <li class="divider"></li>
+                            <li><a href="{{ route('logout') }}" onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                                            <i class="fa fa-power-off"></i>
+                                        {{ __('Logout') }}
+                                </a></li>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                </form>
+                        </ul>
+                    </li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </nav>
+
+
+
+@yield('content')
+@yield('Mensaje')
+
+<script src="{{ asset('js/jquery-1.10.2.js') }}"></script>
+<script src="{{ asset('js/bootstrap.js') }}"></script>
+<script src="{{ asset('js/datatable.js') }}"></script>
+<script src="{{ asset('js/alertify.min.js') }}"></script>
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>>
+<script src="https://cdn.jsdelivr.net/npm/promise-polyfill@8/dist/polyfill.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+</body>
+</html>
+@endauth
